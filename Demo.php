57ab8145266971837f6627f4fefc71ff5c96d243
Jose Alexis Correa valencia <jalexiscv@gmail.com>
2016-01-21 09:31:24 -0500

    Update Demo.php

--- Demo.php
+++ Demo.php
@@ -3,3 +3,16 @@ require_once("Equipos.class.php");
 
 $e=new Equipos();
 $e->crear(array("id"=>1,"so"=>1,"version"=>'1',"imei"=>'21423123123',"serial"=>'123123123',"centro"=>11,"est"=>11));
+
+
+//Consulta multiple.
+$expresion="WHERE `id`='".$id."'";
+$db = new MySQL();
+$sql ="SELECT * FROM `equipos` ".$expresion.";";
+$consulta=$db->sql_query($sql);
+while($fila =$db->sql_fetchrow($consulta)){
+  //Cada ves que se repita el ciclo obtendre una nueva fila
+  print_r($fila);
+};
+$db->sql_close();
+return($fila);
